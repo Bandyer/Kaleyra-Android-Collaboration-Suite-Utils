52245a5e01622d8250f551d1b22cea7fa4970971
Kristiyan Petrov <petrov.kristiyan@gmail.com>
2020-03-31 15:49:17 +0200

    Add the possibility to specify the executor for observerCollection created via factory

--- bandyer-android-common/src/main/java/com/bandyer/android_common/observer/ObserverFactory.kt
+++ bandyer-android-common/src/main/java/com/bandyer/android_common/observer/ObserverFactory.kt
@@ -3,6 +3,8 @@ package com.bandyer.android_common.observer
 import android.os.Looper
 import com.badoo.mobile.util.WeakHandler
 import java.lang.reflect.Proxy
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
 
 /**
  * A factory to create observers given a generic type
@@ -15,7 +17,9 @@ object ObserverFactory {
      * @param handler WeakHandler? handler where the callbacks will be posted
      * @return T the observer collection created
      */
-    inline fun <reified T> createObserverCollection(handler: WeakHandler? = WeakHandler(Looper.getMainLooper())): T where T : ObserverCollection<*> {
+    @JvmOverloads
+    inline fun <reified T> createObserverCollection(handler: WeakHandler? = WeakHandler(Looper.getMainLooper()), executor: ExecutorService? = Executors.newSingleThreadExecutor()): T where T : ObserverCollection<*> {
+        executor?.let { return Proxy.newProxyInstance(T::class.java.classLoader, arrayOf(T::class.java), BaseObserverCollection<T>(handler,it)) as T }
         return Proxy.newProxyInstance(T::class.java.classLoader, arrayOf(T::class.java), BaseObserverCollection<T>(handler)) as T
     }
 }
