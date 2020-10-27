2d058d7ffda3917b5fe126eb03a8fcb6738e93cf
Kristiyan Petrov <kristiyan.petrov@bandyer.com>
2020-10-27 17:07:23 +0100

    Update documentation generation

--- scripts/publish_doc.py
+++ scripts/publish_doc.py
@@ -24,7 +24,10 @@ print "|---> RELEASING API DOC " + sdk_releasing_package
 def copy_and_overwrite(from_path, to_path):
     if os.path.exists(to_path):
         shutil.rmtree(to_path)
-    shutil.copytree(from_path, to_path)
+    try:
+        shutil.copytree(from_path, to_path)
+    except:
+       print("Path not exiting. Removing folder")
 
 #################################################################################
 ########################## CHECK MASTER CHANGES ################################
