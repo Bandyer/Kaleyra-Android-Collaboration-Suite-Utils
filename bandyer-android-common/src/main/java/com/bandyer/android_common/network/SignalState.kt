17431c31d985f9238be9c6c15726e0c7370dbdd2
Stefano <bpbrusad@gmail.com>
2021-11-12 18:07:03 +0100

    add observers and audio manager

---
+++ bandyer-android-common/src/main/java/com/bandyer/android_common/network/SignalState.kt
@@ -0,0 +1,31 @@
+package com.bandyer.android_common.network
+
+/**
+ * Cell signal state
+ */
+enum class SignalState {
+    /**
+     * n o n e
+     */
+    NONE,
+
+    /**
+     * p o o r
+     */
+    POOR,
+
+    /**
+     * m o d e r a t e
+     */
+    MODERATE,
+
+    /**
+     * g o o d
+     */
+    GOOD,
+
+    /**
+     * g r e a t
+     */
+    GREAT
+}
