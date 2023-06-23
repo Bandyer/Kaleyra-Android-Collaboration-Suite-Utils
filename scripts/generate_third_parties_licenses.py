215792465bdaa0375f1f18c9c73c7873d73b43e0
Federico Marin <federico.marin@kaleyra.com>
2023-06-23 12:56:29 +0200

    Update version catalog usage

--- scripts/generate_third_parties_licenses.py
+++ scripts/generate_third_parties_licenses.py
@@ -4,7 +4,6 @@ os.chdir("../")
 print("|---> GENERATING THIRD PARTIES LICENCES FILE ...")
 
 os.system("./gradlew generateLicenseReport")
-os.system("cp collaboration-suite-utils/build/licenses/licenses.json .")
-os.system("mv licenses.json THIRD_PARTY_LICENSES.json")
+os.system("cp collaboration-suite-utils/build/licenses/licenses.json THIRD_PARTY_LICENSES.txt")
 
 print("|---> GENERATED THIRD PARTIES LICENCES FILE to THIRD_PARTY_LICENSES.txt")
