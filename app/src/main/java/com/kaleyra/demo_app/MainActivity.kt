a6221d882ac1bb1705ef2de95696d7377fdc8e93
Federico Marin <federico.marin@kaleyra.com>
2023-09-29 12:48:24 +0200

    Fix R import

--- app/src/main/java/com/kaleyra/demo_app/MainActivity.kt
+++ app/src/main/java/com/kaleyra/demo_app/MainActivity.kt
@@ -6,7 +6,6 @@ package com.kaleyra.demo_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.kaleyra.collaboration_suite_utils.demo_app.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
