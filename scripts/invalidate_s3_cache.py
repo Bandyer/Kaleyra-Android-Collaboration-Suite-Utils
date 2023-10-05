b03535690a673ce6867f6d6f16b007d063603a8d
Federico Marin <federico.marin@kaleyra.com>
2023-10-05 13:50:51 +0200

    Fix invalidate maven cache script

--- scripts/invalidate_s3_cache.py
+++ scripts/invalidate_s3_cache.py
@@ -6,7 +6,7 @@ AWS_ACCESS_KEY = sys.argv[1]
 AWS_SECRET_ACCESS_KEY = sys.argv[2]
 AWS_CF_DISTRIBUTION_ID = sys.argv[3]
 AWS_PATH = sys.argv[4]  # such as releases/
-PACKAGE_NAME = sys.argv[5]  # such as com.kaleyra.library
+PACKAGE_NAME = sys.argv[5]  # such as com.bandyer.library
 PACKAGE_VERSION = sys.argv[6]  # such as 1.0
 
 aws_package_path = PACKAGE_NAME.replace(".", "/")
@@ -21,11 +21,11 @@ root_path = AWS_PATH + "/" + aws_package_path + "/"
 
 package_split = PACKAGE_NAME.split('.')
 
-foldersToInvalidate = [ '/?list-type=2&delimiter=/&prefix='+ AWS_PATH +'/&max-keys=50',
-                         '/?list-type=2&delimiter=/&prefix='+ AWS_PATH + '/' + package_split[0] + '/&max-keys=50',
-                         '/?list-type=2&delimiter=/&prefix='+ AWS_PATH + '/' + package_split[0] + '/' + package_split[1] +'/&max-keys=50',
-                         '/?list-type=2&delimiter=/&prefix='+ root_path +'&max-keys=50', # invalidate necessary for website indexing
-                         '/?list-type=2&delimiter=/&prefix='+ root_path + PACKAGE_VERSION + '/&max-keys=50', # invalidate necessary for website indexing
+foldersToInvalidate = [ '/?list-type=2&delimiter=/&prefix='+ AWS_PATH +'/&max-keys=1000',
+                         '/?list-type=2&delimiter=/&prefix='+ AWS_PATH + '/' + package_split[0] + '/&max-keys=1000',
+                         '/?list-type=2&delimiter=/&prefix='+ AWS_PATH + '/' + package_split[0] + '/' + package_split[1] +'/&max-keys=1000',
+                         '/?list-type=2&delimiter=/&prefix='+ root_path +'&max-keys=1000', # invalidate necessary for website indexing
+                         '/?list-type=2&delimiter=/&prefix='+ root_path + PACKAGE_VERSION + '/&max-keys=1000', # invalidate necessary for website indexing
                         "/" +  root_path + "maven-metadata.*",
                         "/" +  root_path + PACKAGE_VERSION + '/*']
 
